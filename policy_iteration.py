98c49f5861713d3210e5744de5b1bfc4ecf50f4e
Fitti <dev@fitti.io>
2022-05-11 13:52:55 +0200

    Update

--- policy_iteration.py
+++ policy_iteration.py
@@ -1,37 +1,49 @@
 from math import factorial, exp
 
 
-def initialize(S: list, A: dict):
+def initialize(S: dict, A: dict):
     V = {s: 0 for s in S}
-    pi = {s: A[s[0]] for s in S}
+    pi = {s: A[s][0] for s in S}
     return V, pi
 
 
-def get_value(s, a, S, R, p, gamma, V):
-    return sum(p(s_next, r, s, a)*(r+gamma*V[s_next])
-               for s_next in S for r in R)
+def get_value(s, a, S, p, gamma, V):
+    rets = []
+    s_pa = [S[s][0]-a, S[s][1]+a]
+    for s_preq in S:
+        for s_pret in S_prets[str(s_pa)][s_preq]:
+            r = get_reward(S[s_preq], s_pret, a)
+            prob = probs[str(s_pa)][s_preq][str(s_pret)]
+            ret = r+gamma*V[s_preq]
+            rets.append(prob*ret)
+    return sum(rets)
 
 
-def eval(S, A, R, p, gamma, V, pi, d_min):
+def eval(S, A, p, gamma, V, pi, d_min):
     while True:
         d = 0
         for s in S:
+            print(f"Evaluating {s}  ", end='\r')
             v = V[s]
-            V[s] = get_value(s, pi[s], S, R, p, gamma, V)
+            V[s] = get_value(s, pi[s], S, p, gamma, V)
             d = max(d, abs(v-V[s]))
         if d < d_min:
             break
     return V
 
 
-def improve(S, A, R, p, gamma, V, pi):
+def improve(S, A, p, gamma, V, pi):
     stable = True
     for s in S:
-        old = get_value(s, pi[s], S, R, p, gamma, V)
-        for a in A:
-            if get_value(s, a, S, R, p, gamma, V) > old:
-                pi[s] = a
-                stable = False
+        old = V[s]
+        print(f"Finding best action for state {s}  ", end='\r')
+        for a in A[s]:
+            new = get_value(s, a, S, p, gamma, V)
+            if new > old:
+                V[s] = new
+                if pi[s] != a:
+                    stable = False
+                    pi[s] = a
     return stable, pi
 
 
@@ -39,54 +51,72 @@ def poisson(lmbd, n):
     return ((lmbd**n) / factorial(n)) * exp(-lmbd)
 
 
-def post_return_prob(s_pret, s, a):
-    s_pa = [s[0]-a, s[0]+a]
-    if s_pret[0] < s_pa[0] or s_pret[1] < s_pa[0]:
-        return 0
-    else:
-        p = poisson(3, s_pret[0]-s_pa[0]) * poisson(2, s_pret[1]-s_pa[1])
-        return p
+def post_return_prob(s_pret, s_pa):
+    p = poissons["3"][s_pret[0]-s_pa[0]] * poissons["2"][s_pret[1]-s_pa[1]]
+    return p
 
 
 def post_request_prob(s_preq, s_pret):
-    if s_preq[0] > s_pret[0] or s_preq[1] > s_pret[1]:
-        return 0
-    else:
-        p = poisson(3, s_pret[0]-s_preq[0]) * poisson(4, s_pret[1]-s_preq[1])
-        return p
+    p = poissons["3"][s_pret[0]-s_preq[0]] * poissons["4"][s_pret[1]-s_preq[1]]
+    return p
 
 
-def reward_prob(r, s_preq, s_pret, a):
+def get_reward(s_preq, s_pret, a):
     r_0 = 10 * (s_pret[0] - s_preq[0])
-    r_1 = 10 * (s_pret[1] - s_pret[1])
-    r_true = r_0 + r_1 - (2 * a)
-    return 1 if r == r_true else 0
+    r_1 = 10 * (s_pret[1] - s_preq[1])
+    return r_0 + r_1 - (2 * a)
 
 
-def p_4(S, s_next, r, s, a):
-    return sum(post_request_prob(s_next, s_pret)*post_return_prob(s_pret, s, a)
-               for s_pret in S)
+def p_4(s_next, s_pret, s_pa):
+    probs = []
+    preq = post_request_prob(s_next, s_pret)
+    pret = post_return_prob(s_pret, s_pa)
+    probs.append(preq*pret)
+    return sum(probs)
 
 
 if __name__ == "__main__":
     S = {f"{n1}, {n2}": [n1, n2] for n1 in range(21) for n2 in range(21)}
-    A = {f"{n1}, {n2}": [a for a in range(1, min(5, 20-n2, n1)+1)] for n1 in
+    A = {f"{n1}, {n2}": [a for a in range(min(5, 20-n2, n1)+1)] for n1 in
          range(21) for n2 in range(21)}
-    R = list(range(-10, 401, 2))
     gamma = 0.9
-
-    def p(s_next, r, s, a):
-        if s_next == a:
-            r_true = 1 if s_next == "3" else 0
-            return r if r_true == r else 0
-        else:
-            return 0
 
     V, pi = initialize(S, A)
 
+    lmbds = [2, 3, 4]
+    ns = [i for i in range(21)]
+    poissons = {str(lmbd): [poisson(lmbd, n) for n in ns] for lmbd in lmbds}
+
+    print("Building tables")
+    S_prets = {}
+    probs = {}
+    for s in S:
+        print(f"State: {s}  ", end='\r')
+        for a in A[s]:
+            s_pa = [S[s][0]-a, S[s][1]+a]
+            S_prets[str(s_pa)] = {}
+            probs[str(s_pa)] = {}
+            for s_preq in S:
+                lb0 = max(s_pa[0], S[s_preq][0])
+                lb1 = max(s_pa[1], S[s_preq][1])
+                S_pret = [[n1, n2] for n1 in range(lb0, 21) for n2 in
+                          range(lb1, 21)]
+                probs[str(s_pa)][s_preq] = {}
+                for s_pret in S_pret:
+                    probs[str(s_pa)][s_preq][str(s_pret)] = p_4(S[s_preq],
+                                                                s_pret,
+                                                                s_pa)
+                S_prets[str(s_pa)][s_preq] = S_pret
+    print("Tables complete")
+
     stable = False
+    i = 0
     while not stable:
-        V = eval(S, A, R, p, gamma, V, pi, 0.000001)
-        stable, pi = improve(S, A, R, p, gamma, V, pi)
+        i += 1
+        print(f"Step: {i}")
+        V = eval(S, A, probs, gamma, V, pi, 0.1)
+        print("Policy evaluated.")
+        stable, pi = improve(S, A, probs, gamma, V, pi)
+        print("Policy updated.")
 
     print(pi)
